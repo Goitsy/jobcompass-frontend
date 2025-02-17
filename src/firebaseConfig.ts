15cd3288178b65bfd4984e94b7902aae2e8645be
GDMontsho <montshogoitseone@gmail.com>
2025-02-17 10:16:50 +0100

    firebase config

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,52 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+
+const signInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(auth, googleProvider);
+    console.log("User Info:", result.user);
+    return result.user;
+  } catch (error) {
+    console.error("Google Sign-In Error:", error);
+  }
+};
+
+const signInWithFacebook = async () => {
+  try {
+    const result = await signInWithPopup(auth, facebookProvider);
+    console.log("User Info:", result.user);
+    return result.user;
+  } catch (error) {
+    console.error("Facebook Sign-In Error:", error);
+  }
+};
+
+export {
+  auth,
+  googleProvider,
+  facebookProvider,
+  signInWithPopup,
+  signInWithGoogle,
+  signInWithFacebook,
+};
