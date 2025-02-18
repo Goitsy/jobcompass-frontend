8b1eb42347080cd2ddd1292359108cfd5bc1a4f3
GDMontsho <montshogoitseone@gmail.com>
2025-02-18 07:46:48 +0100

    update deps

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) {
-              return "react-vendor";
-            }
-            if (id.includes("chart.js")) {
-              return "chartjs-vendor";
-            }
-            return "vendor";
-          }
-        },
-      },
-    },
+    sourcemap: true, // Enable source maps for easier debugging
+
     chunkSizeWarningLimit: 1000,
   },
 });
